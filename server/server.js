eef83b64a22e962eb1185a7882ef2bbce2e02a42
Dylan Carter <dylancarter@Dylans-MacBook-Pro.local>
2018-07-09 13:58:32 +1000

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -6,7 +6,10 @@
 // A) LOAD MODULES - BUILT IN
 // -------------------------
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
 // -------------------------
 
 // B) LOAD MODULES - APP SPECIFIC
@@ -26,14 +29,26 @@ const express = require('express');
 // -------------------------
 
 
-
 // ESTABLISH LISTENING SERVER
 // -------------------------
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+    console.log(`New user connected. User id: ${socket.id}`);
+
+    socket.on('disconnect', () => {
+        console.log(`User has been disconnected. User id: ${socket.id}`);
+    });
+
+});
+
+server.listen(port, () => {
     console.log(`Server has been established on port ${port}`);
 });
 // -------------------------
